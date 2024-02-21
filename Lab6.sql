4eed5e52cf80bb979f3f8f31daa7cd9bfa532690
Harsh Shah <112803355+12hms12@users.noreply.github.com>
2024-02-21 16:23:17 +0530

    Delete Lab6.sql

--- Lab6.sql
+++
@@ -1,8 +0,0 @@
-set serveroutput on
-create procedure proc1(p1 varchar(10),p2 varchar(10))
-
-begin
-    call dbms_output.put('p1 = '||p2)
-    call dbms_output.put('p2 = '||p2)
-    call dbms_output.new_line
-end;
